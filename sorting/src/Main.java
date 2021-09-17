198ef1fca881e0402bfdde5ed072be743df64870
Joy1618 <43145075+Joy1618@users.noreply.github.com>
2021-09-17 01:16:13 -0700

    create insertion sort

---
+++ sorting/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //This is the unsorted array
+        int[] unsorted = new int[] {2,1,4,3};
+        System.out.println(Arrays.toString(unsorted));
+    }
+
+    //this is for insertion sort
+    public static int[] InsertionSort(int[] arr){
+        for(int i = 1; i < arr.length; i++) {
+            int k = arr[i];
+            int pos = i;
+            while(pos > 0 && k < arr[pos-1]) {
+                arr[pos] = arr[pos-1];
+                pos--;
+            }
+            arr[pos] = k;
+        }
+        return arr;
+    }
+
+}
