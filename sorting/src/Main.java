6735bef6fddba1e2b5f051f77880e80a0352e4bd
Joy1618 <43145075+Joy1618@users.noreply.github.com>
2021-09-17 01:24:41 -0700

    sort array in ascending order

--- sorting/src/Main.java
+++ sorting/src/Main.java
@@ -6,6 +6,10 @@ public class Main {
         //This is the unsorted array
         int[] unsorted = new int[] {2,1,4,3};
         System.out.println(Arrays.toString(unsorted));
+
+        //This is the array sorted in ascending order
+        int[] sorted2 = InsertionSort(unsorted);
+        System.out.println(Arrays.toString(sorted2));
     }
 
     //this is for insertion sort
